dec334553519c5bf768a74f3728381a3f3c24e0a
marius <m.gzegozevskis@gmail.com>
2020-10-01 17:44:05 +0300

    waeawe

---
+++ cmake-build-debug/CMakeFiles/Garazas.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Garazas.dir/main.cpp.obj"
+  "Garazas.exe"
+  "Garazas.exe.manifest"
+  "Garazas.pdb"
+  "libGarazas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Garazas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include<bits/stdc++.h>
+
+struct Gedimas {
+    std::string pavadinimas;
+    bool arSugedes;
+    int id;
+    double procentaliai;
+};
+
+struct Autoservisas {
+    std::vector<Gedimas> gedimai;
+
+    void papildytiGedimuSarasa(Gedimas gedimas);
+};
+
+
+struct Klientas {
+    Autoservisas autoservisas;
+    std::string vardas;
+    std::vector<Gedimas> gedimai;
+
+    void pateiktiGedima(Gedimas g);
+
+    void gedimuSarasas();
+};
+
+
+struct Automobilis {
+    std::string modelis;
+    Klientas klientas;
+};
+
+
+struct Ataskaita {
+
+};
+
+struct Meistras {
+    void tvarkytiAutomobili(Automobilis automobilis);
+};
+
+
+int main() {
+
+    Autoservisas autoservisas;
+
+    Gedimas variklio;
+    variklio.pavadinimas = "Variklio gedimas!";
+    variklio.arSugedes = true;
+    variklio.id = 1;
+    variklio.procentaliai = 0.4;
+
+    Gedimas pakabos;
+    pakabos.pavadinimas = "Pakabos gedimas!";
+    pakabos.arSugedes = true;
+    pakabos.id = 2;
+    pakabos.procentaliai = 1.0;
+
+    autoservisas.papildytiGedimuSarasa(variklio);
+    autoservisas.papildytiGedimuSarasa(pakabos);
+
+    Klientas klientas;
+    klientas.vardas = "Petras";
+
+    std::cout << "=========================================\n";
+    int input = 1;
+    int index = 1;
+    while (input != 0) {
+        std::cout << "Pasirinkite gedimus:\n";
+        index = 1;
+        for (Gedimas g: autoservisas.gedimai) {
+            std::cout << "\t" << index << ". " << g.pavadinimas << std::endl;
+            index++;
+        }
+        std::cout << "3. Atspausdinti Gedimu Sarasa.\n";
+        std::cout << "0. ISEITI.\n";
+
+        std::cin >> input;
+
+        switch (input) {
+            case 0:
+                break;
+            case 1:
+                klientas.pateiktiGedima(autoservisas.gedimai[0]);
+                break;
+            case 2:
+                klientas.pateiktiGedima(autoservisas.gedimai[1]);
+                break;
+            case 3:
+                klientas.gedimuSarasas();
+                break;
+            default:
+                std::cout << "NETINKAMA IVESTIS!\n";
+                break;
+
+        }
+
+    }
+    // std::cout << "=========================================\n";
+
+
+
+
+
+    std::cout << "=========================================\n";
+
+
+    // cout <<
+
+
+    return 0;
+}
+
+void Autoservisas::papildytiGedimuSarasa(Gedimas gedimas) {
+    gedimai.push_back(gedimas);
+}
+
+
+void Meistras::tvarkytiAutomobili(Automobilis automobilis) {
+
+}
+
+void Klientas::pateiktiGedima(Gedimas g) {
+
+    gedimai.push_back(g);
+
+
+
+
+
+}
+
+void Klientas::gedimuSarasas() {
+
+    std::cout << "================GEDIMU SARASAS=========================\n";
+
+    for (Gedimas g: gedimai) {
+        std::cout << "pav: " << g.pavadinimas << "arSugedes: " << g.arSugedes << "bukle: " << g.procentaliai
+                  << std::endl;
+    }
+
+    std::cout << "=========================================\n";
+
+
+}
